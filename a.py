c6d8cca7c5f7cc8200cdd75c4f720dbb88fa3a39
Sebastian Nunez <02snunez@gmail.com>
2023-02-14 11:00:45 -0500

    binary search

--- a.py
+++ a.py
@@ -1,50 +1,19 @@
-# def binary(n, k):
-#     result = []
-#     binary_helper(n, "", result, k)
-#     return result
+def binary_search_recursive(nums, target):
 
+    def helper(lower, upper):
+        if lower > upper:
+            return -1
 
-# def binary_helper(n: int, possible: str, lst: list, k):
-#     if len(possible) == n:
+        mid = lower + (upper - lower) // 2
 
-#         return
-#     num_ones = possible.count("1")  # pruwn the branch )stop recursing
-#     if num_ones < k:
-#         lst.append(possible)
-#     binary_helper(n, possible + "0", lst)
-#     binary_helper(n, possible + "1", lst)
+        if target < nums[mid]:
+            return helper(lower, mid - 1)
+        elif target > nums[mid]:
+            return helper(mid + 1, upper)
+        else:
+            return mid
 
+    return helper(0, len(nums) - 1)
 
-# print(binary(3, 2))
-
-def distribute_books(books, k):
-  target = sum(books) / k
-
-  def helper(i, subset):
-    subset_sum = sum(subset)
-    if subset_sum == target:
-      print(subset)
-      return 1
-
-    if subset_sum > target:
-      return 0
-
-    if i >= len(books):
-      return 0
-
-    # read
-    subset.append(books[i])
-    read = helper(i + 1, subset)
-
-    # not read
-    subset.pop() # backtrack
-    not_read = helper(i + 1, subset)
-
-    return read + not_read
-
-  num_subsets = helper(0, list())
-  return num_subsets >= k
-
-
-# print(distribute_books([8,15,11,20,8], 2)) # True [8, 15, 8] and [11, 20] -> 31
-print(distribute_books([6,1,3,2,2,4,1,2], 3)) # True
+print(binary_search_recursive([1,2,3,4,5], 4)) # 3
+print(binary_search_recursive([1,2,3,4,5], 8)) # -1
--- tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-    subCopy = subset[:]
